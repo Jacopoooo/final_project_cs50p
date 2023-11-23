e7d7a6d0f9ee9432f0e6568a5a76a4e45f33bcb5
unknown <tecchiojacopo07@gmail.com>
2023-11-23 19:28:45 +0100

    Aggiornato comportamento della funzione [callback]
    Ora la funzione callback manipola gli attributi all'interno della classe [BackgroundCalls] invece di gestire variabili globali

--- background_listening.py
+++ background_listening.py
@@ -4,7 +4,7 @@ import time
 import sys
 
 terminate_recording = False
-content = []
+m = sr.Microphone()
 
 
 class BackgroundCalls(Recognizer):
@@ -14,10 +14,11 @@ class BackgroundCalls(Recognizer):
             microphone, sr.Microphone), 'Il microfono deve essere di tipo [sr.Microphone]'
 
         self._microphone = microphone
+        self._content = []
 
         super().__init__()
 
-    def background_listening(self, text_content: list, duration, message: str):
+    def background_listening(self, duration, message: str):
         text_data = ''
 
         with self._microphone as source:
@@ -31,23 +32,72 @@ class BackgroundCalls(Recognizer):
 
         for _ in range(duration):
             if terminate_recording:
-                text_content.clear()
+                self._content.clear()
                 sys.exit('Registrazione terminata')
             time.sleep(1)
 
         stop_recognition()
 
-        for sentence in text_content:
+        for sentence in self._content:
             text_data += sentence + ' '
 
-        if len(text_content) != 0:
-            text_content.clear()
+        if len(self._content) != 0:
+            self._content.clear()
 
         return text_data
 
+    # Inizio funzionalità di personalizzazione del pdf
 
+    def get_notes_title(self, duration=15, message='Che titolo vuoi dare alle tue note?'):
+        notes_title = self.background_listening(
+            duration, message).lower()
+
+        return notes_title.strip()
+
+    def get_prompt(self, duration=15, message='Che prompt vuoi dare a ChatGPT?'):
+        prompt = self.background_listening(
+            duration, message).lower()
+
+        return prompt.strip()
+
+    def get_pdf_type(self, duration=10, message='Che tipo di pdf vuoi creare? '):
+        text = self.background_listening(
+            duration, message).lower()
+
+        if 'note' in text:
+            return 'notes'
+        elif 'gpt' in text or 'chatgpt' in text:
+            return 'gpt'
+        else:
+            raise ValueError(
+                'Puoi creare solo file di note o che sfruttano chatgpt')
+
+    def get_pdf_name(self, duration=12, message='Come vuoi chiamare il tuo pdf?'):
+        counter = 0
+
+        while True:
+            try:
+                pdf_name = self.background_listening(
+                    duration, message).lower()
+
+                if counter != 0:
+                    print('Inserisci un nuovo nome valido')
+
+                if not pdf_name:
+                    raise OSError
+
+                with open(pdf_name + '.pdf', 'w'):
+                    pass
+
+            except OSError:
+                counter += 1
+                print('Nome invalido')
+            else:
+                return pdf_name.strip()
+
+
+# Funzione chiamata dal thread in background
 def callback(recognizer: sr.Recognizer, audio_data: sr.AudioData):
-    global content
     global terminate_recording
 
     try:
@@ -62,71 +112,15 @@ def callback(recognizer: sr.Recognizer, audio_data: sr.AudioData):
     else:
         if 'termina la registrazione' in text:
             terminate_recording = True
-        content.append(text)
+        calls.__dict__['_content'].append(text)
 
 
-def get_pdf_type(calls_object: BackgroundCalls, text_content, duration=10, message='Che tipo di pdf vuoi creare? '):
-    text = calls_object.background_listening(
-        text_content, duration, message).lower()
-
-    if 'note' in text:
-        return 'notes'
-    elif 'gpt' or 'chatgpt' in text:
-        return 'gpt'
-    else:
-        raise ValueError(
-            'Puoi creare solo file di note o che sfruttano chatgpt')
-
-
-def get_pdf_name(calls_object: BackgroundCalls, text_content, duration=12, message='Come vuoi chiamare il tuo pdf?'):
-    counter = 0
-
-    while True:
-        try:
-            pdf_name = calls_object.background_listening(
-                text_content, duration, message).lower()
-
-            if counter != 0:
-                print('Inserisci un nuovo nome valido')
-
-            if not pdf_name:
-                raise OSError
-            
-            with open(pdf_name + '.pdf', 'w'):
-                pass
-            
-        except OSError:
-            counter += 1
-            print('Nome invalido')
-        else:
-            return pdf_name.strip()
-
-
-def get_notes_title(calls_object: BackgroundCalls, text_content, duration=15, message='Che titolo vuoi dare alle tue note?'):
-    notes_title = calls_object.background_listening(text_content, duration, message).lower()
-
-    return notes_title.strip()
-
-def get_prompt(calls_object: BackgroundCalls, text_content, duration=15, message='Che prompt vuoi dare a ChatGPT?'):
-    prompt = calls_object.background_listening(text_content, duration, message).lower()
-
-    return prompt.strip()
-
+calls = BackgroundCalls(m)
 
 
 def main():
-    global content
-
-    m = sr.Microphone()
-    calls = BackgroundCalls(m)
-
-    pdf_type = get_pdf_type(calls, content)
-    pdf_name = get_pdf_name(calls, content)
-    if pdf_type == 'notes':
-        notes_title = get_notes_title(calls, content)
-    else:
-        prompt = get_prompt(calls, content)
-
+    print(calls.get_pdf_type())
+    print(calls.get_pdf_name())
 
 
 main()
